88ba65b23d128b3965446a4b175495ca489237c5
OakmanByte <antonskebaber@gmail.com>
2025-03-19 00:19:07 +0100

    fix

--- assets/script.js
+++ assets/script.js
@@ -6,22 +6,22 @@ const countdown = () => {
     const targetDate = new Date(2025, 3, 1, 0, 0, 0, 0).getTime(); // April 1st, 2025 ms
     const currentDate = new Date().getTime(); //ms
 
-    var timeLeft = targetDate - currentDate; //ms
+    let timeLeft = targetDate - currentDate; //ms
 
     if (timeLeft < 0) {
         document.getElementById("countdown").innerHTML = "WE ARE GOING TO BARCELONA!!";
         return;
     }
 
-    var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
     timeLeft %= (1000 * 60 * 60 * 24);
-    var hours = Math.floor(timeLeft / (1000 * 60 * 60));
+    let hours = Math.floor(timeLeft / (1000 * 60 * 60));
     timeLeft %= (1000 * 60 * 60);
-    var minutes = Math.floor(timeLeft / (1000 * 60));
+    let minutes = Math.floor(timeLeft / (1000 * 60));
     timeLeft %= (1000 * 60);
-    var seconds = Math.floor(timeLeft / (1000));
+    let seconds = Math.floor(timeLeft / (1000));
 
-    var countdownString = `${days} : ${hours} : ${minutes} : ${seconds}`;
+    let countdownString = `${days} : ${hours} : ${minutes} : ${seconds}`;
 
     document.getElementById("countdown").innerHTML = countdownString;
 
