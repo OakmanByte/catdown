ef7c78c8de58a36e502cd85a1925906d4f8c42fb
OakmanByte <antonskebaber@gmail.com>
2025-03-18 23:58:43 +0100

    test

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+// Countdown logic example
+const countdown = () => {
+    const targetDate = new Date(2025, 3, 1, 0, 0, 0, 0).getTime(); //ms
+    const currentDate = new Date().getTime(); //ms
+
+    const timeLeft = targetDate - currentDate; //ms
+
+    if (timeLeft < 0)
+        document.getElementById("countdown").innerHTML = "WE ARE GOING TO BARCELONA!!";
+
+
+    var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    timeLeft %= (1000 * 60 * 60 * 24);
+    var hours = Math.floor(timeLeft / (1000 * 60 * 60));
+    timeLeft %= (1000 * 60 * 60);
+    var minutes = Math.floor(timeLeft / (1000 * 60));
+    timeLeft %= (1000 * 60);
+    var seconds = Math.floor(timeLeft / (1000));
+
+    var countdownString = `${days} : ${hours} : ${minutes} : ${seconds}`;
+
+    document.getElementById("countdown").innerHTML = countdownString;
+
+}
